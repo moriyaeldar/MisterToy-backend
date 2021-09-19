581e6abc6cc42026922245ae9cd2f7b23c6b6149
Moriya Eldar <moriyaz1@gmail.com>
2021-09-19 21:55:29 +0300

    <commit-messgae>

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 'dbURL': 'mongodb://localhost:27017',
+  'dbURL': 'mongodb://Moriya:0527710115@cluster0.pw6or.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://Moriya:0527710115@cluster0.pw6or.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
