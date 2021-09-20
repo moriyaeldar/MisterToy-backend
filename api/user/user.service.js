792bd54b7413066999899d717dab14ecc20113ab
Moriya Eldar <moriyaz1@gmail.com>
2021-09-20 13:35:54 +0300

    12

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -2,7 +2,6 @@
 const dbService = require('../../services/db.service')
 const logger = require('../../services/logger.service')
 const reviewService = require('../review/review.service')
-import { socketService, SOCKET_EVENT_USER_UPDATED } from './socket.service'
 
 const ObjectId = require('mongodb').ObjectId
 
